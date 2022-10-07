8760367f7895c16e044fcc5fe09ef01a730fef3e
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2022-10-07 08:33:56 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.7.20"
+}
+
+kotlin {
+    jvmToolchain {
+        this.languageVersion.set(JavaLanguageVersion.of(17))
+    }
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
 }
 
 tasks.test {
@@ -17,6 +23,16 @@ repositories {
 }
 
 dependencies {
+    // https://junit.org/junit5/docs/current/user-guide/
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://kotlinlang.org/api/latest/kotlin.test/kotlin.test/
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest:kotest-assertions-core:5.4.1")
+
+    // https://kotest.io/docs/assertions/assertions.html
+    testImplementation("io.kotest:kotest-assertions-core:5.5.0")
+
+    // https://mockk.io/
+    testImplementation("io.mockk:mockk:1.13.2")
 }
