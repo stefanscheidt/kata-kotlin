3195b813fbf0b884f5384dbb84546e722bf2308c
Nabil Abdel-Hafeez <7283535+987Nabil@users.noreply.github.com>
2022-10-21 11:01:40 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test/kotlin/pokerhand/PokerHandTest.kt

--- src/test/kotlin/pokerhand/PokerHandTest.kt
+++ src/test/kotlin/pokerhand/PokerHandTest.kt
@@ -1,6 +1,6 @@
 package pokerhand
 
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.comparables.shouldBeLessThan
 import org.junit.jupiter.api.Test
 
 
@@ -8,6 +8,6 @@ class PokerHandTest {
 
     @Test
     fun `Compare 2 Cards`() {
-       Card(Suit.Club,3).shouldBeLessThan(Card(Suit.Diamond,4))
+        Card(Suit.Club, 3).shouldBeLessThan(Card(Suit.Diamond, 4))
     }
 }
