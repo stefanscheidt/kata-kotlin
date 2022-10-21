e382c51880eac1807f9e83fdfff98da341794f65
Sebastian.Barz <Sebastian.Barz@rewe-digital.com>
2022-10-21 11:27:57 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/kotlin/pokerhand/PokerHand.kt

--- src/main/kotlin/pokerhand/PokerHand.kt
+++ src/main/kotlin/pokerhand/PokerHand.kt
@@ -17,6 +17,32 @@ data class ComparisonResult(val result: Int)
 
 data class Hand(val card1: Card, val card2: Card, val card3: Card, val card4: Card, val card5: Card)
 
+// Var 1
+enum class HandType {
+    StraightFlush,
+    FourOfAKind
+}
+
+// Var 2
+data class HandRank(
+    val handType: HandType,
+    val highCards: List<CardValue>
+)
+
+// Var 3
+sealed class HandType2 {
+    data class StraightFlush(
+        val suit: Suit,
+        val cards: List<CardValue>
+    ) : HandType2()
+
+    data class FourOfAKind(
+        val highCardFour: CardValue,
+        val highCardSingle: CardValue
+    ): HandType2()
+}
+
+
 // comparisonResult.value < 0 means hand1 loses,
 // comparisonResult.value > 0 means hand1 wins,
 // comparisonResult.value == 0 means draw
