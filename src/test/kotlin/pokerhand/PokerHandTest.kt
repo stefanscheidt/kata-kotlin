39db4a595fe4802ee15a800c428650c3523df225
Stefan Scheidt <stefan.scheidt@rewe-digital.com>
2022-10-21 10:09:30 +0200

    Add initial test test

---
+++ src/test/kotlin/pokerhand/PokerHandTest.kt
@@ -0,0 +1,13 @@
+package pokerhand
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+
+class PokerHandTest {
+
+    @Test
+    internal fun `give me a good name`() {
+        true shouldBe true
+    }
+}
