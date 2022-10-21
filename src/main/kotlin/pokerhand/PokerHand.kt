6b5d5661a94c906695cecb5d30859734fb16999d
Thomas Hoffmann <thomas.hoffmann@rewe-digital.com>
2022-10-21 11:09:22 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/kotlin/pokerhand/PokerHand.kt

--- src/main/kotlin/pokerhand/PokerHand.kt
+++ src/main/kotlin/pokerhand/PokerHand.kt
@@ -13,5 +13,11 @@ data class Card(val suit: Suit, val value: CardValue) : Comparable<Card> {
 
 }
 
+data class ComparisonResult(val result: Int)
+
 data class Hand(val card1: Card, val card2: Card, val card3: Card, val card4: Card, val card5: Card)
 
+// comparisonResult.value < 0 means hand1 loses,
+// comparisonResult.value > 0 means hand1 wins,
+// comparisonResult.value == 0 means draw
+fun compare(hand1: Hand, hand2: Hand): ComparisonResult = TODO()
