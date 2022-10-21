34a451745a9784058bbdff5ca2bec68314e09203
Mischa Dieterle <mischa.dieterle@rewe-digital.com>
2022-10-21 10:30:18 +0200

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/main/kotlin/pokerhand/PokerHand.kt

--- src/main/kotlin/pokerhand/PokerHand.kt
+++ src/main/kotlin/pokerhand/PokerHand.kt
@@ -8,3 +8,5 @@ enum class Suit {
 typealias CardValue = Int
 
 data class Card(val suit: Suit, val value: CardValue)
+
+data class Hand(val card1: Card,val card2: Card,val card3: Card,val card4: Card,val card5: Card)
